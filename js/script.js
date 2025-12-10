f9fb09d034126e6bc30bf5edd6dfdbf711aa5d30
Jaime <jaimeamayasanchez02@gmail.com>
2025-12-10 15:34:59 +0100

    MENU HAM ICONO

--- js/script.js
+++ js/script.js
@@ -1,15 +1,19 @@
 
 $(document).ready(function() {
-    
+    //menú hamburguesa
     const $menuToggle = $('.menu-toggle');
     const $mainNav = $('.main-nav');
+    const $icon = $menuToggle.find('i'); 
 
     $menuToggle.on('click', function() {
-        
         $mainNav.toggleClass('active');
         
+        if ($mainNav.hasClass('active')) {
+            $icon.removeClass('fa-bars').addClass('fa-xmark');
+        } else {
+            $icon.removeClass('fa-xmark').addClass('fa-bars');
+        }
     });
-
 
     //animación scroll
     $('a[href*="#"]').on('click', function(e) {
