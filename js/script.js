f2b914e8f4df9dd82f73036ee059548434fc6659
Jaime <jaimeamayasanchez02@gmail.com>
2025-12-11 22:17:02 +0100

    SLIDESHOW/RESPONSIVE

--- js/script.js
+++ js/script.js
@@ -29,3 +29,59 @@ $(document).ready(function() {
     });
 
 });
+
+
+// SLIDE SHOW
+let slideIndex = 1;
+let autoSlideInterval;
+
+showSlides(slideIndex);
+startAutoSlide();
+
+//FLECHAS
+function plusSlides(n) {
+    stopAutoSlide();
+    showSlides(slideIndex += n);
+    startAutoSlide();
+}
+
+//PUNTOS
+function currentDotSlide(n) {
+    stopAutoSlide();
+    showSlides(slideIndex = n + 1);
+    startAutoSlide();
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    
+    if (slides.length === 0) return;
+
+    if (n > slides.length) {slideIndex = 1}    
+    if (n < 1) {slideIndex = slides.length}
+    
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    
+    slides[slideIndex-1].style.display = "block";  
+    if (dots.length > 0) {
+        dots[slideIndex-1].className += " active";
+    }
+}
+
+function startAutoSlide() {
+    clearInterval(autoSlideInterval); 
+    autoSlideInterval = setInterval(function(){ 
+        plusSlides(1); 
+    }, 4000);
+}
+
+function stopAutoSlide() {
+    clearInterval(autoSlideInterval);
+}
